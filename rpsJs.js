338536c50894fadabfed07478b8f94d0f65655a1
Gummadi-Mohan-Krishna <gmohankrishnawork@gmail.com>
2024-12-29 12:18:31 +0530

    upload from gitbash

---
+++ rpsJs.js
@@ -0,0 +1,72 @@
+let choices=document.querySelectorAll(".choice");
+let userScore=document.querySelector("#uscore");
+let compScore=document.querySelector("#cscore");
+let us=0;
+let cs=0;
+const msg=document.querySelector("#message");
+
+choices.forEach(choice => {
+    choice.addEventListener("click",()=>{
+        const userChoice=choice.getAttribute("id");
+        playGame(userChoice);
+    })
+});
+
+const compSelection=()=>{
+    obj=["rock","paper","scissors"];
+    ind=Math.floor(Math.random()*3);
+    return obj[ind];
+}
+
+const playGame=(userChoice)=>{
+    const compChoice=compSelection();
+    gameResults(userChoice,compChoice);
+}
+const gameResults=(userChoice,compChoice)=>{
+    if(userChoice===compChoice){
+        msg.innerText="GAME DRAW!!!";
+        msg.style.backgroundColor = "#081b31";
+    }
+    else if(userChoice==="rock"){
+        if(compChoice==="scissors"){
+            msg.innerText="YOU WON";
+            msg.style.backgroundColor = "green";
+            us++;
+            userScore.innerText=us;
+        }
+        else{
+            msg.innerText="YOU LOST";
+            msg.style.backgroundColor = "red";
+            cs++;
+            compScore.innerText=cs;
+        }
+    }
+    else if(userChoice==="paper"){
+        if(compChoice==="rock"){
+            msg.innerText="YOU WON";
+            msg.style.backgroundColor = "green";
+            us++;
+            userScore.innerText=us;
+        }
+        else{
+            msg.innerText="YOU LOST";
+            msg.style.backgroundColor = "red";
+            cs++;
+            compScore.innerText=cs;
+        }
+    }
+    else{
+        if(compChoice==="paper"){
+            msg.innerText="YOU WON";
+            msg.style.backgroundColor = "green";
+            us++;
+            userScore.innerText=us;
+        }
+        else{
+            msg.innerText="YOU LOST";
+            msg.style.backgroundColor = "red";
+            cs++;
+            compScore.innerText=cs;
+        }
+    }
+}
